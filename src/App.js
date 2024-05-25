2fdd1b6b73a42995dd05a720337b9d138c64c5fd
Ágatha <agathamagnusg@hotmail.com>
2024-05-25 14:47:09 -0300

    🌐 add react-router-dom to handle with routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/Login/LoginPage'
+import RegisterPage from './pages/Register/RegisterPage'
 import './styles/stylesGlobals.scss'
 
 export default function App() {
   return (
-    <LoginPage></LoginPage>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   )
 }
